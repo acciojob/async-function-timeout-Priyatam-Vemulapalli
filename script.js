06e48b42bb74530b40b9ff109708fb2788975d5c
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-10-30 10:36:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click",async() => {
+	const text = document.getElementById("text").value;
+	const delay = parseInt(document.getElementById("delay").value);
+	if (isNan(delay) || delay < 0){
+		document.getElementById("output").textContent = "Please enter a valid delay.";
+		return ;
+	}
+	await displayMessageAfterDelay(text,delay);
+});
+
+async function displayMessageAfterDelay(text, delay) {
+  await new Promise((resolve) => setTimeout(resolve, delay));
+  document.getElementById("output").textContent = text;
+}
